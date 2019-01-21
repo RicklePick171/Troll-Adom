639aca6511235ec10e8305e6f03b9b0d6d79724f
TH <tal.hd03@gmail.com>
2019-01-21 10:14:32 +0200

    העלתי מיין קדמן

---
+++ Tal hd/mainMasterPage/mainMasterPage/sevivon/sevivon/sevivonJS.js
@@ -0,0 +1,22 @@
+﻿var spin0="spinner/spinNG.png";
+var spin1="spinner/spinGH.png";
+var spin2="spinner/spinHP.png";
+var spin3="spinner/spinPN.png";
+setInterval("start()",350)
+function show(str)
+{
+    pic.src=str;
+}
+var i=1;
+function start()
+{
+    j=i%4;
+    switch(j)
+    {
+        case 0: show(spin0);break;
+        case 1: show(spin1);break;
+        case 2: show(spin2);break;
+        case 3: show(spin3);break;
+    }
+    i++;
+}
